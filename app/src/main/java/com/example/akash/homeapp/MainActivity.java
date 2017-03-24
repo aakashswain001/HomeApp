f129d7e3150cb0db10a169a8e4a890a94ef58de5
aakashswain001@gmail.com <Akash1234>
2017-03-25 00:17:19 +0530

    //working on off with time

--- app/src/main/java/com/example/akash/homeapp/MainActivity.java
+++ app/src/main/java/com/example/akash/homeapp/MainActivity.java
@@ -1,13 +1,104 @@
 package com.example.akash.homeapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Created by root on 24/3/17.
+ */
+
+public class MainActivity extends Activity {
+    private EditText time;
+    private Button on,off;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        time = (EditText)findViewById(R.id.ed);
+        time.setText("0");
+        on = (Button)findViewById(R.id.on);
+        off = (Button)findViewById(R.id.off);
+        on.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertToDatabase(time.getText().toString(),"1");
+            }
+        });
+        off.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertToDatabase(time.getText().toString(),"0");
+            }
+        });
+    }
+
+
+    private void insertToDatabase(final String time, final String status){
+        class SendPostReqAsyncTask extends AsyncTask<String, Void, String> {
+            @Override
+            protected String doInBackground(String... params) {
+                String paramTime = params[0];
+                String paramStatus = params[1];
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+                nameValuePairs.add(new BasicNameValuePair("time",time));
+                nameValuePairs.add(new BasicNameValuePair("status", status));
+
+                try {
+                    HttpClient httpClient = new DefaultHttpClient();
+                    HttpPost httpPost = new HttpPost(
+                            "http://studentsforhire.in/test1_inp");
+                    httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+                    HttpResponse response = httpClient.execute(httpPost);
+
+                    HttpEntity entity = response.getEntity();
+
+
+                } catch (ClientProtocolException e) {
+
+                } catch (IOException e) {
+
+                }
+                return "success";
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                super.onPostExecute(result);
+
+                Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+                TextView textViewResult = (TextView) findViewById(R.id.tttt);
+                textViewResult.setText("Inserted");
+            }
+        }
+        SendPostReqAsyncTask sendPostReqAsyncTask = new SendPostReqAsyncTask();
+        sendPostReqAsyncTask.execute(time,status);
     }
 }
